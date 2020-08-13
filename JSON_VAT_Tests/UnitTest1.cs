20975aea364c2f2fbd9e3d39c63c961e68b9c93a
Kateřina Trčková <trckovakaterina@gmail.com>
2020-08-13 16:31:06 +0200

    Fix of Country class methods. And changes in tests.

--- JSON_VAT_Tests/UnitTest1.cs
+++ JSON_VAT_Tests/UnitTest1.cs
@@ -1,23 +1,36 @@
-using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using JSON_VAT_Project;
 using System.Collections.Generic;
+using System.Net;
+using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+using JSON_VAT_Project;
+using NUnit.Framework;
 
 namespace JSON_VAT_Tests
 {
-    [TestClass]
     public class CountryRateCalculationsTests
+
     {
-
-        string vatJson = JsonProcessing.getJson("https://euvatrates.com/rates.json");
-
-        List<Country> countries = JsonProcessing.getListOfCountries(vatJson);
 
         [Test]
 
         // MethodName_StateUnderTest_ExpectedBehavior
         public void GetHighestRateCountries_OneCountry_GetCountryWithHighestStandartRate()
         {
+            WebClient client = new WebClient();
+            string vatJson = JsonProcessing.getJson("https://euvatrates.com/rates.json");
+
+            List<Country> countries = new List<Country>();
+
+            const string RegexPattern = @"""[A-Z]{2}""[ :]+((?=\[)\[[^]]*\]|(?=\{)\{[^\}]*\}|\""[^""]*\"")";
+            foreach (var item in Regex.Matches(vatJson, RegexPattern, RegexOptions.Multiline))
+            {
+                var countrySubstringStartIndex = item.ToString().IndexOf('{');
+                var countrySubstring = item.ToString().Substring(countrySubstringStartIndex);
+                countries.Add(JsonConvert.DeserializeObject<Country>(countrySubstring));
+            }
+
             // Arrange
             string expectedResult = "Hungary";
 
